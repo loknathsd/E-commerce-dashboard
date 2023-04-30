698f75ceaaa54aab741875c5e8df82c71fe8e913
loknathsd <loknathsds66@gmail.com>
2023-05-01 00:10:30 +0600

    register page done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
 
   return (
     <div className="">
-      <Login />
+      {/* <Login /> */}
+      <Register />
     </div>
   );
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,21 +3,21 @@ import { FcGoogle } from 'react-icons/fc';
 import { FaFacebookF } from 'react-icons/fa';
 import { useForm } from 'react-hook-form';
 
-const Login = () => {
+const Register = () => {
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
     const onSubmit = data => console.log(data);
 
     return (
         <div className='bg-gray-100 w-full h-[100vh] flex justify-center'>
-            <div className=' w-[50%] shadow-lg  h-3/4 bg-white mt-10 rounded-md text-center '>
+            <div className=' w-[50%] shadow-lg  h-5/6 bg-white mt-10 rounded-md text-center '>
                 <div className='font-serif' >
-                    <h1 className='mt-12 text-3xl font-serif'>SignIn To Dashboard</h1>
+                    <h1 className='mt-12 text-3xl font-serif'>SignUp To Dashboard</h1>
                     <div className='flex justify-center gap-3 mt-10 mb-8'>
                         <button className='border-2 border-gray-30 w-72  rounded-md py-2 flex justify-center items-center align-middle '>
-                            <FcGoogle className='mr-3 text-2xl' /> SignIn with Google
+                            <FcGoogle className='mr-3 text-2xl' /> SignUp with Google
                         </button>
                         <button className='border-2 border-gray-30 w-72  rounded-md py-2 flex justify-center items-center align-middle '>
-                            <FaFacebookF className='mr-3 text-2xl text-blue-600' /> SignIn with Facebook
+                            <FaFacebookF className='mr-3 text-2xl text-blue-600' /> SignUp with Facebook
                         </button>
                     </div>
                     <div className='flex justify-center items-center gap-2'>
@@ -25,25 +25,30 @@ const Login = () => {
                         <span className='text-gray-500 font-medium text-lg'>Or</span>
                         <hr className='w-1/3 border-1 border-gray-300' />
                     </div>
-                    <form onSubmit={handleSubmit(onSubmit)}>
+                    <form onSubmit={handleSubmit(onSubmit)} >
+                        <div className='flex justify-center text-left'>
+                            <div className='w-[86%]'>
+                                <label >Name</label><br />
+                                <input className='mt-2 border-2 border-gray-30 w-full  py-2 rounded-md px-3' {...register("name")} required />
+                            </div>
+                        </div>
                         <div className='flex justify-center gap-3 text-left mt-4 mb-6 font-serif'>
                             <div>
                                 <label htmlFor="">Email</label><br />
-                                <input className=' mt-2 border-2 border-gray-30 w-72 py-2 rounded-md px-3' {...register("email", { required: true })} />
+                                <input className=' mt-2 border-2 border-gray-30 w-72 py-2 rounded-md px-3' {...register("email")} required />
                             </div>
                             <div>
                                 <label htmlFor="">Password</label><br />
-                                <input className='mt-2 border-2 border-gray-30 w-72 py-2 rounded-md px-3' {...register("password", { required: true })} />
+                                <input className='mt-2 border-2 border-gray-30 w-72 py-2 rounded-md px-3' {...register("password")} required />
                             </div>
                         </div>
-
-                        <input className='cursor-pointer border-2 border-gray-30 rounded-md text-white w-[86%] py-2 bg-sky-500' type="submit" value="SignIn" />
+                        <input className='cursor-pointer border-2 border-gray-30 rounded-md text-white w-[86%] py-2 bg-sky-500' type="submit" value="SignUp" />
                     </form>
-                    <p className='my-5 text-sm text-gray-400'> Don't have an account?{" "} <a href="d"> Create an account</a></p>
+                    <p className='my-5 text-sm text-gray-400'>Do you already have an account? {" "} <a href="d"> Log in</a></p>
                 </div>
             </div>
         </div>
     );
 };
 
-export default Login;
+export default Register;

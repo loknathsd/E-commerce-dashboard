df8c4e23d7118b1518779a83c3adcf388d30aedd
loknathsd <loknathsds66@gmail.com>
2023-05-01 11:55:17 +0600

    register page responsived mobile and tab view

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -9,15 +9,15 @@ const Register = () => {
     const onSubmit = data => console.log(data);
 
     return (
-        <div className='bg-gray-100 w-full h-[100vh] flex justify-center'>
-            <div className=' w-[50%] shadow-lg  h-5/6 bg-white mt-10 rounded-lg text-center '>
+        <div className='bg-gray-100 w-full lg:h-[100vh] flex justify-center'>
+            <div className=' lg:w-[50%] md:w-[60%] shadow-lg  h-5/6 bg-white mt-10 rounded-lg text-center md:p-8 max-sm:p-8'>
                 <div className='font-serif' >
                     <h1 className='mt-12 text-3xl font-serif capitalize'>Create an account</h1>
-                    <div className='flex justify-center gap-3 mt-10 mb-8'>
-                        <button className='border-2 border-gray-30 w-72  rounded-md py-2 flex justify-center items-center align-middle '>
+                    <div className='lg:flex md:flex justify-center gap-3 mt-10 mb-8'>
+                        <button className='border-2 border-gray-30 w-72 md:w-52  rounded-md py-2 flex justify-center items-center align-middle '>
                             <FcGoogle className='mr-3 text-2xl' /> SignUp with Google
                         </button>
-                        <button className='border-2 border-gray-30 w-72  rounded-md py-2 flex justify-center items-center align-middle '>
+                        <button className=' max-sm:mt-5 border-2 border-gray-30 w-72 md:w-52  rounded-md py-2 flex justify-center items-center align-middle '>
                             <FaFacebookF className='mr-3 text-2xl text-blue-600' /> SignUp with Facebook
                         </button>
                     </div>
@@ -27,20 +27,20 @@ const Register = () => {
                         <hr className='w-1/3 border-1 border-gray-300' />
                     </div>
                     <form onSubmit={handleSubmit(onSubmit)} >
-                        <div className='flex justify-center text-left'>
-                            <div className='w-[86%]'>
+                        <div className='lg:flex justify-center text-left'>
+                            <div className='lg:w-[86%] '>
                                 <label >Name</label><br />
                                 <input className='mt-2 border-2 border-gray-30 w-full  py-2 rounded-md px-3' {...register("name")} required />
                             </div>
                         </div>
-                        <div className='flex justify-center gap-3 text-left mt-4 mb-6 font-serif'>
+                        <div className='lg:flex md:flex justify-center gap-3 text-left mt-4 mb-6 font-serif'>
                             <div>
                                 <label htmlFor="">Email</label><br />
-                                <input className=' mt-2 border-2 border-gray-30 w-72 py-2 rounded-md px-3' {...register("email")} required />
+                                <input className=' mt-2 border-2 border-gray-30 w-72 md:w-52 py-2 rounded-md px-3' {...register("email")} required />
                             </div>
                             <div>
                                 <label htmlFor="">Password</label><br />
-                                <input className='mt-2 border-2 border-gray-30 w-72 py-2 rounded-md px-3' {...register("password")} required />
+                                <input className='mt-2 border-2 border-gray-30 w-72 md:w-52 py-2 rounded-md px-3' {...register("password")} required />
                             </div>
                         </div>
                         <input className='cursor-pointer border-2 border-gray-30 rounded-md text-white w-[86%] py-2 bg-sky-500' type="submit" value="SignUp" />

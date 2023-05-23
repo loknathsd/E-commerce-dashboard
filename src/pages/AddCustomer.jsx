2da021f72e0f6665c5ffbbf6cb8da0d2a0a0b07a
loknathsd <loknathsds66@gmail.com>
2023-05-23 23:59:13 +0600

    add customer page

--- src/pages/AddCustomer.jsx
+++ src/pages/AddCustomer.jsx
@@ -3,7 +3,7 @@ import AddCustomerForm from '../components/AddCustomerForm/AddCustomerForm';
 
 const AddCustomer = () => {
     return (
-        <div className='w-4/5 bg-white mx-auto mt-10'>
+        <div className='w-4/5 bg-white mx-auto mt-5'>
             <AddCustomerForm />
         </div>
     );
